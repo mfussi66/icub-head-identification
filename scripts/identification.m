12bbbb0f44783ce755ce823e3eae5eade8a901fa
Mattia Fussi <mattia.fussi@iit.it>
2022-04-08 13:32:20 +0200

    wip

--- scripts/identification.m
+++ scripts/identification.m
@@ -7,8 +7,8 @@ ur = data_head_openloop.get('pid_roll').Values;
 up = resample(up, yp.Time);
 ur = resample(ur, yr.Time);
 
-kt = 0;
-jp = 1e-8;
-kv = 0;
-l = 1e-8;
-b = 0.00001;
+kt = 0.1;
+jp = 1e-2;
+kv = 0.1;
+l = 1e-2;
+b = 0.01;
